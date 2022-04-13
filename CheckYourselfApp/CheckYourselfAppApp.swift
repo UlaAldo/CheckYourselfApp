7dbda36c8e38dc2b362681ab48eed81a5a8a278a
Ula <1295593@mail.ru>
2022-04-13 22:26:19 +0800

    Initial Commit

---
+++ CheckYourselfApp/CheckYourselfAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckYourselfAppApp.swift
+//  CheckYourselfApp
+//
+//  Created by Юлия Алдохина on 13/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CheckYourselfAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
